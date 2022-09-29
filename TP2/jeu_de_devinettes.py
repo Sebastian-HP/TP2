eb2b9f6b34cfa10c5c8d5a2c00044226dbd3bcb8
Sebastian-HP <hasbunpovedas@educ.college-montreal.qc.ca>
2022-09-29 10:44:31 -0400

    Enregistrement de fichier

--- TP2/jeu_de_devinettes.py
+++ TP2/jeu_de_devinettes.py
@@ -5,13 +5,41 @@ Code joue un jeu de devinette evec l'usager ou il doiit deviner un nombre choisi
 '''
 
 
-from random import *
+from random import randint
 
 player_guess = int(input("Numero"))
 computer_generated_number = randint(0,100)
+score = 0
+continue_playing = True
 
-def correct_answer:
+def answer_is_bigger_than():
+    global score
+    print("Votre essai est plus grand que le numero\n\n")
+    score += 1
 
-def answer_is_bigger_than:
 
-def anwer_is_smaller_than:
+def answer_is_smaller_than():
+    global score
+    print("Votre essai est plus petit que le numero\n\n")
+    score += 1
+
+
+def correct_answer():
+    global score
+    global continue_playing
+    print("Vous avez choisi le bon numero\nVotre score est: " + score)
+    continue_
+
+
+while continue_playing == True :
+    if player_guess < computer_generated_number:
+        answer_is_smaller_than()
+
+    elif player_guess > computer_generated_number:
+        answer_is_bigger_than()
+
+    elif player_guess == computer_generated_number:
+        correct_answer()
+
+    else:
+        print("error")
