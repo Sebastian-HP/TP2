9adf1123a98e34899430c8a3b9263bc0392038b4
Sebastian-HP <hasbunpovedas@educ.college-montreal.qc.ca>
2022-09-30 09:16:50 -0400

    Modification

--- TP2/jeu_de_devinettes.py
+++ TP2/jeu_de_devinettes.py
@@ -1,29 +1,26 @@
 """
 Projet fait par Sebastian Hasbun Poveda le 26-09-2022
 Groupe 403
-Code joue un jeu de devinette evec l'usager ou il doiit deviner un nombre choisi par le code entre 0 et 100
+Code joue un jeu de devinette evec l'usager ou il doit deviner un nombre choisi par le code entre 0 et 100
 """
 
 
 from random import randint
-
 score = 0
 continue_playing = True
 
 
-def answer_is_bigger_than():
+def check_answer(answer):
     global score
-    print("Votre essai est plus grand que le numero\n")
-    score += 1
-
-
-def answer_is_smaller_than():
-    global score
-    print("Votre essai est plus petit que le numero\n")
+    print(f"Votre essai est plus {answer} que le numero\n")
     score += 1
 
 
 def correct_answer():
+    """
+
+    :return: None
+    """
     global continue_playing, answer_found, score
     print(f"Vous avez choisi le bon numero\nVotre score est: {score}")
     continue_playing_answer = input("Voulez vous continuer? y/n")
@@ -41,10 +38,10 @@ while continue_playing:
         player_guess = int(input("\nDevinez:"))
 
         if player_guess < computer_generated_number:
-            answer_is_smaller_than()
+            check_answer("petit")
 
         elif player_guess > computer_generated_number:
-            answer_is_bigger_than()
+            check_answer("grand")
 
         elif player_guess == computer_generated_number:
             correct_answer()
